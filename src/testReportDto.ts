9ba6a2bbc9c61ecd5abe7fa2a710466b68d48ab6
Adrian Pikul <adr.pikul@gmail.com>
2025-08-28 16:19:55 +0200

    update dtos

--- src/testReportDto.ts
+++ src/testReportDto.ts
@@ -1,7 +1,11 @@
 import type { TestResponse } from "./testResponseDto"
 
+// Key is in format 2025-07-01_02-10-04_branchName_githash
+type TestTypeResult = Record<string, TestResponse>
+
 export type TestReportDto = {
-    smokeTests: TestResponse[],
-    uiUatTests: TestResponse[],
-    pricingOverride: TestResponse[],
+    smokeTests: TestTypeResult,
+    uiUatTests: TestTypeResult,
+    pricingOverride: TestTypeResult,
 }
+
